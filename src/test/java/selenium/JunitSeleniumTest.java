13808a43ddacd14fa54ba3a66cbda83716737aa3
AshutoshYShepHertz <ashutosh.yadav@shephertz.com>
2015-11-17 02:51:16 +0530

    Update JunitSeleniumTest.java

--- src/test/java/selenium/JunitSeleniumTest.java
+++ src/test/java/selenium/JunitSeleniumTest.java
@@ -21,10 +21,10 @@ public class JunitSeleniumTest {
     public void myTest() throws Exception {
     	
     	WebDriver driver = new RemoteWebDriver(
-                                new URL("http://107.189.67.133/wd/hub/"), 
+                                new URL("http://23.92.254.237/wd/hub/"), 
                                 DesiredCapabilities.firefox());
         
-        driver.get("http://107.189.66.250:8080/");
+        driver.get("http://23.92.254.233:8080/");
         
         
         // RemoteWebDriver does not implement the TakesScreenshot class
